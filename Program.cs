310aba286570ce374d679bb507c4c29d565a8335
elanurisli <islielanur1@gmail.com>
2026-07-21 16:21:48 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿//DEĞİŞKENLER
+
+var kdvOrani = 1.18;
+var ürünA = 5000;
+var ürünB = 6000;
+var ürünC = 7000;
+var ürünD = 5500;  
+
+Console.WriteLine(ürünA*kdvOrani);           //ürünA 
+Console.WriteLine(ürünB*kdvOrani);          //ürünB
+Console.WriteLine(ürünC*kdvOrani);         //ürünC 
+Console.WriteLine(ürünD*kdvOrani);        //ürünD  
+
+
+//var sayi = 20;
+//var urunAdi ="Samsung S23";
+
+
+var urun_adi = "Samsung S23";
+
+//BOŞLUK İÇERMEZ DEĞİŞKEN ADLARI
+// DEĞİŞKEN ADLARI SAYI İLE BAŞLAYAMAZ
+var sayi2 = 30;
+var sayi3 = 50;
+
+var fiyat = 1000.25;
+var Fiyat = 1000.25;
+
+//BÜYÜK-KÜÇÜK HARF DUYARLILIĞI VARDIR.(CAMELCASE)
+
+var satistaMi = true;
+var ogrenciNo = 1234;
+
+//Console.WritLine(sayi);
+Console.WriteLine(urun_adi);
+Console.WriteLine(sayi2);
+Console.WriteLine(sayi3);
+Console.WriteLine(fiyat);
+System.Console.WriteLine(Fiyat);
+System.Console.WriteLine(satistaMi);
+System.Console.WriteLine(ogrenciNo); 
+
+
+
+/* 
+
+  Uygulama1 : Bir öğrencinin aşağadaki bilgileri için gerekli değişkenleri oluşturunuz.
+
+  öğrenci adı
+  öğrenci soyadı 
+  öğrenci ad ve soyad 
+  öğrenci numarası 
+  öğrenci cinsiyet 
+  öğrenci tc kimlik 
+  öğrenci doğum yılı 
+  öğrenci adres bilgileri 
+  öprenci yaşı
+
+
+
+  Uygulama2 : Aşağıdaki ürünlerin toplam bilgisini hesaplayınız.
+
+  ürün 1 = 50 tl 
+  ürün 2 = 60.5 tl 
+  ürün 3 = 356.45 tl  
+
+
+  */
+
+  var ogrenciAdi = "Elanur ";
+  var ogrenciSoyadi = "İşli";
+  var adSoyad = ogrenciAdi + ogrenciSoyadi ;
+  var ogrenciNo ="12";
+  var ogrenciTc = "12121212121";
+  var ogrenciDogumYili = 2006;
+  var ogrenciAdresBilgisi ="İstanbul/Sultanbeyli";
+  var ogrenciYasi = 2026 - ogrenciDogumYili;
+  
+
+  System.Console.WriteLine(ogrenciAdi);
+  System.Console.WriteLine(ogrenciSoyadi);
+  System.Console.WriteLine(adSoyad);
+  System.Console.WriteLine(ogrenciNo);
+  System.Console.WriteLine(ogrenciTc );
+  System.Console.WriteLine(ogrenciDogumYili);
+  System.Console.WriteLine(ogrenciAdresBilgisi);
+  System.Console.WriteLine(ogrenciYasi);
+
+  var ürün1 = 50 ;
+  var ürün2 = 60.5;
+  var ürün3 = 365.45 ;
+  var toplam =ürün1 + ürün2 + ürün3;
+  System.Console.WriteLine(toplam);
+  
+  
+
+
+ 
+
+  
